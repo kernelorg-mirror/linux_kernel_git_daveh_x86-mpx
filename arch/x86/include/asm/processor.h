dc9d0559be901361f1f1d272ce210d15c91552ac
Dave Hansen <dave.hansen@intel.com>
2014-11-14 13:10:09 -0800

    [v3] x86 mpx: rename xsave structures registers
    From: Dave Hansen <dave.hansen@linux.intel.com>
    
    Changes from V2:
     * remove '_struct' from some of the structure names
    
    Changes from V1:
     * name it 'bndreg' instead plain 'bnd'
    
    --
    
    From: Dave Hansen <dave.hansen@linux.intel.com>
    
    Consider the bndX MPX registers.  There 4 registers each
    containing a 64-bit lower and a 64-bit upper bound.  That's 8*64
    bits and we declare it thusly:
    
    	struct bndregs_struct {
    		u64 bndregs[8];
    	}
    
    Let's say you want to read the upper bound from the MPX register
    bnd2 out of the xsave buf.  You do:
    
    	bndregno = 2;
    	upper_bound = xsave_buf->bndregs.bndregs[2*bndregno+1];
    
    That kinda sucks.  Every time you access it, you need to know:
    1. Each bndX register is two entries wide in "bndregs"
    2. The lower comes first followed by upper.  We do the +1 to get
       upper vs. lower.
    
    This replaces the old definition.  You can now access them
    indexed by the register number directly, and with a meaningful
    name for the lower and upper bound:
    
    	bndregno = 2;
    	xsave_buf->bndreg[bndregno].ub;
    
    It's now *VERY* clear that there are 4 registers.  The programmer
    now doesn't have to care what order the lower and upper bounds
    are in, and it's harder to get it wrong.
    
    Also pull some of the redundant '_struct's out of the struct
    names.
    
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: x86@kernel.org
    Cc: "H. Peter Anvin" <hpa@linux.intel.com>
    Cc: Qiaowei Ren <qiaowei.ren@intel.com>
    Cc: "Yu, Fenghua" <fenghua.yu@intel.com>

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -374,11 +374,12 @@ struct lwp_struct {
 	u8 reserved[128];
 };
 
-struct bndregs_struct {
-	u64 bndregs[8];
+struct bndreg {
+	u64 lower_bound; /* aka LB in the SDM */
+	u64 upper_bound; /* aka UB */
 } __packed;
 
-struct bndcsr_struct {
+struct bndcsr {
 	u64 cfg_reg_u;
 	u64 status_reg;
 } __packed;
@@ -394,8 +395,8 @@ struct xsave_struct {
 	struct xsave_hdr_struct xsave_hdr;
 	struct ymmh_struct ymmh;
 	struct lwp_struct lwp;
-	struct bndregs_struct bndregs;
-	struct bndcsr_struct bndcsr;
+	struct bndreg bndreg[4];
+	struct bndcsr bndcsr;
 	/* new processor state extensions will go here */
 } __attribute__ ((packed, aligned (64)));
 
